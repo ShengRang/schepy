1d2304434bf14faa6486b98eafb386cf6b1b39bc
ShengRang <541628498@qq.com>
2016-05-26 19:16:03 +0800

    厘清list和lexp-seq调用函数的区别

--- runtime.py
+++ runtime.py
@@ -14,33 +14,55 @@ from util import colorful
 class BuildIn(object):
 
     @staticmethod
-    def mul(args):
+    def add(*args):
+        return reduce(op.add, args, 0)
+
+    @staticmethod
+    def mul(*args):
         return reduce(op.mul, args, 1)
 
+    @staticmethod
+    def sub(*args):
+        return reduce(op.sub, args, 0)
 
-def define(env, symbol, exp):
-    pass
 
-
-class Env(dict):
+class Env(object):
 
     def __init__(self, outer=None):
         self.outer = outer
+        self.dynamic_bind = dict()
+        self._dict = dict()
         super(Env, self).__init__()
+
+    def __getitem__(self, item):
+        if self.dynamic_bind.get(item, False):
+            return self._dict[item]()
+        return self._dict[item]
+
+    def __setitem__(self, key, value):
+        self._dict[key] = value
 
     def find(self, name):
         """
         沿着作用于链查找name的value
         """
-        return self[name] if (name in self) else self.outer.find(name)
+        try:
+            return self[name] if (name in self._dict) else self.outer.find(name)
+        except AttributeError as e:
+            print(colorful('{0} 在作用域链上不存在!'.format(name), 'Red'))
+            return 'not-define-yet'
+
+    def update(self, *args, **kwargs):
+        return self._dict.update(*args, **kwargs)
 
     @staticmethod
     def std_env():
         env = Env()
-        env.update(vars(math))
+        # env.update(vars(math))
         env.update({
-            '+': sum, '-': op.sub, '*': BuildIn.mul, '/': op.div,
+            '+': BuildIn.add, '-': BuildIn.sub, '*': BuildIn.mul, '/': op.div,
             '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq,
+            'define': partial(define, env),
             'abs':     abs,
             'append':  op.add,
             'apply':   apply,
@@ -66,17 +88,6 @@ class Env(dict):
         return env
 
 
-class Expression(object):
-
-    def __init__(self, params, body, env, etype):
-        self.params, self.body, self.env = params, body, env
-        self.etype = etype
-
-    def __call__(self, *args):
-        # return eval(self.body, Env(self.params, args, self.env))
-        pass
-
-
 class ParseHandler(object):
 
     def __init__(self):
@@ -84,22 +95,30 @@ class ParseHandler(object):
         self.exps = []
 
     def shift(self, token):
-        # if token[1] != 'no-sense':
-            # print(colorful('移近 {0} {1}'.format(token[0], token[1]), "Cyan"))
+        print(colorful('移近 {0} {1}'.format(token[0], token[1]), "Cyan"))
         self.exps.append(SExp(stype=token[0], value=token[1]))
 
     def reduce(self, grammar):
         """
         规约动作, grammar为规约用的产身世
         """
-        # print('利用规则' + colorful('%s -> %s' % (grammar[0], ' '.join(grammar[1])), "Green"))
+        print('利用规则' + colorful('%s -> %s' % (grammar[0], ' '.join(grammar[1])), "Green"))
         if grammar[0] == 'start':
             self.ast = self.exps[0]
             return
         new_node = SExp(stype=grammar[0])
         new_node.child = self.exps[-len(grammar[1]):]
-
         self.exps = self.exps[:-len(grammar[1])] + [new_node]
+
+
+def dynamic_exp(sexp, env):
+    return sexp.calc_value(env)
+
+
+def define(env, symbol, sexp):
+    env[symbol] = partial(dynamic_exp, sexp, env)
+    env.dynamic_bind[symbol] = True
+    return env[symbol]
 
 
 class SExp(object):
@@ -115,16 +134,19 @@ class SExp(object):
         self.value = None      # real value
         self.child = []
         self.parent = parent
+        # self.static = (stype != "identifier")   #非静态表达式
+
+    @property
+    def static(self):
+        if self.stype == "identifier":
+            return True
+        for node in self.child:
+            if node.static:
+                return True
+        return False
 
     def append(self, node):
         self.child.append(node)
-
-    def parser_handler(self):
-        """
-        语法分析时的 handler, 用于生成语法树
-        :return:
-        """
-        pass
 
     def calc_value(self, env=Env.std_env()):
         """
@@ -132,20 +154,24 @@ class SExp(object):
         :param env: 表达式所在环境
         :return:表达式的值
         """
-        if self.value:
+        if self.static and self.value:
             return self.value
+        res = None
         if self.stype == 'number':
-            self.value = int(self._value)
+            res = int(self._value)
         elif self.stype == 'string':
-            self.value = self._value
+            res = self._value
         elif self.stype == 'identifier':
-            self.value = env.find(self._value)
+            # define(env, self._value, self)
+            res = env.find(self._value)
         elif self.stype == 'op':
-            self.value = env.find(self._value)
+            res = env.find(self._value)
         elif self.stype == 'atom' or self.stype == 'func':
-            self.value = self.child[0].calc_value(env)
+            res = self.child[0].calc_value(env)
         elif self.stype == 'lexp':
-            self.value = self.child[0].calc_value(env)
+            res = self.child[0].calc_value(env)
+            # if self.child[0].stype == 'list':
+            #     res = [res, ]
         elif self.stype == 'lexp-seq':
             # ..蜜汁处理. 很重要
             # 对于 lexp-seq -> lexp, 不转list
@@ -154,19 +180,27 @@ class SExp(object):
                 first = [self.child[0].calc_value(env)]
                 if len(self.child[0].child) > 1:
                     first = first[0]
-                self.value = first + [self.child[1].calc_value(env), ]
+                res = first + [self.child[1].calc_value(env), ]
             else:
-                self.value = self.child[0].calc_value(env)
-            # print 'this lexp-seq:', self.value
+                res = self.child[0].calc_value(env)
+                # if self.child[0].stype == 'lexp' and self.child[0].child[0].stype == 'list':
+                #     res = res[0]
+            # print 'this lexp-seq:', res
         elif self.stype == 'list':
             # 一致性处理, lexp-seq 已经做了list处理
-            self.value = self.child[1].calc_value(env)
+            res = self.child[1].calc_value(env)
         elif self.stype == 's-exp':
             func = self.child[1].calc_value(env)
             args = self.child[2].calc_value(env)
-            # print func
-            # print args
-            self.value = func(args)
+            lexp_seq = self.child[2]
+            print func
+            print args
+            if lexp_seq.child[0].stype == 'lexp' and lexp_seq.child[0].child[0].stype == 'list':
+                res = func(args)
+            else:
+                res = func(*args)
         elif self.stype == 'lambda':
             pass
-        return self.value
+        if not self.static:
+            self.value = res
+        return res
